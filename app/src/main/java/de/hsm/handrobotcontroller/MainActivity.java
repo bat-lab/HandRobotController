4057c4da4b9ed932d226d1be33d3a44025a46bb3
Fabrice Dufils Siyapdje <fabrice.dufils.siyapdje@exxeta.com>
2018-12-28 14:49:05 +0100

    * Minor refactoring * Add Finger Controller UI
    - TODO: Styling

--- app/src/main/java/de/hsm/handrobotcontroller/MainActivity.java
+++ app/src/main/java/de/hsm/handrobotcontroller/MainActivity.java
@@ -1,59 +1,61 @@
 package de.hsm.handrobotcontroller;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-import java.io.File;
 
 import im.delight.android.webview.AdvancedWebView;
 
-public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener  {
-	private AdvancedWebView mWebView;
+public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener {
+    private AdvancedWebView mWebView;
+    private final HandRoboterInterface handRoboterInterface = new HandRoboterInterface();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		mWebView = (AdvancedWebView) findViewById(R.id.webview);
-		mWebView.setListener(this, this);
-		mWebView.setGeolocationEnabled(false);
-		mWebView.setMixedContentAllowed(true);
-		mWebView.setCookiesEnabled(true);
-		mWebView.setThirdPartyCookiesEnabled(true);
-		mWebView.setWebViewClient(new WebViewClient() {
+        mWebView = (AdvancedWebView) findViewById(R.id.webview);
+        mWebView.setListener(this, this);
+        mWebView.setGeolocationEnabled(false);
+        mWebView.setMixedContentAllowed(true);
+        mWebView.setCookiesEnabled(true);
+        mWebView.setThirdPartyCookiesEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
 
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				Toast.makeText(MainActivity.this, "Finished loading", Toast.LENGTH_SHORT).show();
-			}
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Toast.makeText(MainActivity.this, "Finished loading", Toast.LENGTH_SHORT).show();
+            }
 
-		});
+        });
 
-		mWebView.setWebChromeClient(new WebChromeClient() {
+        mWebView.setWebChromeClient(new WebChromeClient() {
 
-			@Override
-			public void onReceivedTitle(WebView view, String title) {
-				super.onReceivedTitle(view, title);
-				Toast.makeText(MainActivity.this, title, Toast.LENGTH_SHORT).show();
-			}
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                Toast.makeText(MainActivity.this, title, Toast.LENGTH_SHORT).show();
+            }
 
-		});
+        });
 
-		mWebView.getSettings().setJavaScriptEnabled(true);
-		mWebView.addHttpHeader("X-Requested-With", "");
-		mWebView.loadUrl("file:///android_asset/app/index.html");
-	}
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(handRoboterInterface, "HRI");
+        mWebView.addHttpHeader("X-Requested-With", "");
+        mWebView.loadUrl("file:///android_asset/app/index.html");
+    }
 
-	@SuppressLint("NewApi")
+    @SuppressLint("NewApi")
     @Override
     protected void onResume() {
         super.onResume();
@@ -69,40 +71,46 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
         super.onPause();
     }
 
-	@Override
-	protected void onDestroy() {
-		mWebView.onDestroy();
-		// ...
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        mWebView.onDestroy();
+        // ...
+        super.onDestroy();
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		super.onActivityResult(requestCode, resultCode, intent);
-		mWebView.onActivityResult(requestCode, resultCode, intent);
-		// ...
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        mWebView.onActivityResult(requestCode, resultCode, intent);
+        // ...
+    }
 
-	@Override
-	public void onBackPressed() {
-		if (!mWebView.onBackPressed()) { return; }
-		// ...
-		super.onBackPressed();
-	}
+    @Override
+    public void onBackPressed() {
+        if (!mWebView.onBackPressed()) {
+            return;
+        }
+        // ...
+        super.onBackPressed();
+    }
 
-	@Override
-	public void onPageStarted(String url, Bitmap favicon) { }
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+    }
 
-	@Override
-	public void onPageFinished(String url) { }
+    @Override
+    public void onPageFinished(String url) {
+    }
 
-	@Override
-	public void onPageError(int errorCode, String description, String failingUrl) { }
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+    }
 
-	@Override
-	public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) { }
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+    }
 
-	@Override
-	public void onExternalPageRequest(String url) { }
-
+    @Override
+    public void onExternalPageRequest(String url) {
+    }
 }
