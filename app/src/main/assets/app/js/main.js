d980a9600d80c2bc38057f83bc465669d263abbf
Fabrice Dufils Siyapdje <fabrice.dufils.siyapdje@exxeta.com>
2018-12-28 14:47:38 +0100

    implemet javascript

---
+++ app/src/main/assets/app/js/main.js
@@ -0,0 +1,24 @@
+
+console.log(HRI);
+
+const onFingerClick = (fingerNr) =>
+    HRI.onFingerClick(fingerNr)
+
+const handOpen = () =>
+    HRI.handOpen()
+
+const handClose = () =>
+    HRI.handClose()
+
+const goLeft = () =>
+    HRI.goLeft()
+
+const goRight = () =>
+    HRI.goRight()
+
+const goTop = () =>
+    HRI.goTop()
+
+const goBottom = () =>
+    HRI.goBottom()
+
