d7c49f4b32fb5d00ea5f600d6c91783722d45b14
fabrice.dufils.siyapdje@exxeta.com <fabrice.dufils.siyapdje@extern.mlp.de>
2019-03-20 23:32:55 +0100

    fix padding and handle exception

--- app/src/main/java/de/hsm/handrobotcontroller/HandRoboterInterface.java
+++ app/src/main/java/de/hsm/handrobotcontroller/HandRoboterInterface.java
@@ -20,37 +20,35 @@ public class HandRoboterInterface {
     }
 
     @JavascriptInterface
-    public void handOpen() {
-        System.out.println("Hand open");
-    }
-
-    @JavascriptInterface
-    public void onFingerClick(int fingerNr) {
-        System.out.println(fingerNr);
-    }
-
-    @JavascriptInterface
-    public void goLeft() {
-        System.out.println("Go Left");
-    }
-
-    @JavascriptInterface
     public void onConnectBt() throws IOException {
         this.btService.activate();
     }
 
     @JavascriptInterface
-    public void goRight() {
-        System.out.println("Go Right");
+    public void handOpen() {
+        System.out.println("Hand open");
     }
 
     @JavascriptInterface
-    public void goTop() {
-        System.out.println("go Top");
+    public void onFingerClick(int fingerNr) throws IOException {
+        this.btService.write(this.write(fingerNr));
+        System.out.println(fingerNr);
     }
 
-    @JavascriptInterface
-    public void goBottom() {
-        System.out.println("Go Bottom");
+    private String write(int fingerNr) {
+        switch (fingerNr) {
+            case 1:
+                return "a";
+            case 2:
+                return "b";
+            case 3:
+                return "c";
+            case 4:
+                return "d";
+            case 5:
+                return "e";
+            default:
+                return "";
+        }
     }
 }
